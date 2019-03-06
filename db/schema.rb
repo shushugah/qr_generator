8a75946c933e365ae80021d4e2b630ebf57645f2
Yonatan Miller <yonatan.miller@curated-shopping.com>
2019-03-06 21:42:02 +0100

    use postgres now

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_03_06_193304) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "codes", force: :cascade do |t|
     t.string "url"
     t.string "short_description"
